ce73398387da93b94a4ec65128fb9935cd95400d
Mary Kasparian <kasparian.mary@gmail.com>
2022-08-25 09:11:32 -0500

    Add list of servers and error message

--- script.js
+++ script.js
@@ -2,8 +2,116 @@
 const details = document.querySelector(".details");
 const imgContainer = document.querySelector(".img-container");
 const getUserBtn = document.querySelector(".get-user");
+const errorMsg = document.querySelector("#error-msg");
 
 const url = "https://xivapi.com/character";
+const serverList = [
+  "Adamantoise",
+  "Aegis",
+  "Alexander",
+  "Anima",
+  "Asura",
+  "Atomos",
+  "Bahamut",
+  "Balmung",
+  "Behemoth",
+  "Belias",
+  "Brynhildr",
+  "Cactuar",
+  "Carbuncle",
+  "Cerberus",
+  "Chocobo",
+  "Coeurl",
+  "Diabolos",
+  "Durandal",
+  "Excalibur",
+  "Exodus",
+  "Faerie",
+  "Famfrit",
+  "Fenrir",
+  "Garuda",
+  "Gilgamesh",
+  "Goblin",
+  "Gungnir",
+  "Hades",
+  "Hyperion",
+  "Ifrit",
+  "Ixion",
+  "Jenova",
+  "Kujata",
+  "Lamia",
+  "Leviathan",
+  "Lich",
+  "Louisoix",
+  "Malboro",
+  "Mandragora",
+  "Masamune",
+  "Mateus",
+  "Midgardsormr",
+  "Moogle",
+  "Odin",
+  "Omega",
+  "Pandaemonium",
+  "Phoenix",
+  "Ragnarok",
+  "Ramuh",
+  "Ridill",
+  "Sargatanas",
+  "Shinryu",
+  "Shiva",
+  "Siren",
+  "Tiamat",
+  "Titan",
+  "Tonberry",
+  "Typhon",
+  "Ultima",
+  "Ultros",
+  "Unicorn",
+  "Valefor",
+  "Yojimbo",
+  "Zalera",
+  "Zeromus",
+  "Zodiark",
+  "Spriggan",
+  "Twintania",
+  "Bismarck",
+  "Ravana",
+  "Sephirot",
+  "Sophia",
+  "Zurvan",
+  "HongYuHai",
+  "ShenYiZhiDi",
+  "LaNuoXiYa",
+  "HuanYingQunDao",
+  "MengYaChi",
+  "YuZhouHeYin",
+  "WoXianXiRan",
+  "ChenXiWangZuo",
+  "BaiYinXiang",
+  "BaiJinHuanXiang",
+  "ShenQuanHen",
+  "ChaoFengTing",
+  "LvRenZhanQiao",
+  "FuXiaoZhiJian",
+  "Longchaoshendian",
+  "MengYuBaoJing",
+  "ZiShuiZhanQiao",
+  "YanXia",
+  "JingYuZhuangYuan",
+  "MoDuNa",
+  "HaiMaoChaWu",
+  "RouFengHaiWan",
+  "HuPoYuan",
+  "ShuiJingTa2",
+  "YinLeiHu2",
+  "TaiYangHaiAn2",
+  "YiXiuJiaDe2",
+  "HongChaChuan2",
+  "Alpha",
+  "Phantom",
+  "Raiden",
+  "Sagittarius",
+];
 
 // Functions
 const displayCharacterData = (charData) => {
@@ -24,8 +132,8 @@ const getCharacterData = async (id) => {
 };
 
 // Fetch DATA from FFXIV API
-const getUser = async (name) => {
-  let response = await fetch(`${url}/search?name=${name}&server=Coeurl`);
+const getUser = async (name, server) => {
+  let response = await fetch(`${url}/search?name=${name}&server=${server}`);
   // results property on object is an array - if single result then will just be one object on array
   if (response.ok) {
     let data = await response.json();
@@ -35,13 +143,20 @@ const getUser = async (name) => {
     console.log(id);
     // use lodestone ID to make another fetch request to get detailed character information
     getCharacterData(id);
+  } else {
+    // display an error message
+    if (!serverList.includes(server.toUpperCase())) {
+      // server does not exist
+    }
   }
 };
 
 getUserBtn.addEventListener("click", (event) => {
   // grab input value
-  let name = document.querySelector("input").value;
-  getUser(name.toLowerCase());
+  let name = document.querySelector("#character").value;
+  let server = document.querySelector("#server");
+  // capitalize server name
+  getUser(name.toLowerCase(), server.toLowerCase());
 });
 
 // {
